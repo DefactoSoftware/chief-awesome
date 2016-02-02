69aed67426680b5f4cd422bcbae0f6c65e815f30
Marcel Horlings <marcel.horlings@gmail.com>
2016-02-02 17:15:58 +0100

    Update for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,12 +15,9 @@ Rails.application.configure do
   config.assets.digest = true
   config.log_level = :debug
   config.action_controller.asset_host = ENV.fetch("ASSET_HOST", ENV.fetch("APPLICATION_HOST"))
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = SMTP_SETTINGS
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: ENV.fetch("APPLICATION_HOST") }
 end
 Rack::Timeout.timeout = (ENV["RACK_TIMEOUT"] || 10).to_i
