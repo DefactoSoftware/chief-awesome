f5d3efa4d9561771869b0fdd16e849178681506e
Marcel Horlings <marcel.horlings@gmail.com>
2016-02-03 08:51:46 +0100

    remove rails helper to use only the spec helper

--- spec/features/user_logs_out_spec.rb
+++ spec/features/user_logs_out_spec.rb
@@ -1,14 +1,12 @@
 require "spec_helper"
-require "rails_helper"
-require "support/session_helper"
 
 feature "log out" do
   scenario "user logs out" do
     sign_in_user
     visit root_path
-    
+
     click_link "Log out"
-    
+
     expect(page).to_not have_content("Welcome: foo")
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,22 @@
+ENV["RAILS_ENV"] = "test"
+
 if ENV.fetch("COVERAGE", false)
   require "simplecov"
   SimpleCov.start "rails"
 end
 
+abort("DATABASE_URL environment variable is set") if ENV["DATABASE_URL"]
+
+require "rspec/rails"
 require "webmock/rspec"
 require 'capybara/rspec'
 
+Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |file| require file }
+require File.expand_path("../../config/environment", __FILE__)
+
 module Features
   # Extend this module in spec/support/features/*.rb
+  include Formulaic::Dsl
 end
 
 # http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
@@ -24,6 +33,10 @@ RSpec.configure do |config|
   config.example_status_persistence_file_path = "tmp/rspec_examples.txt"
   config.order = :random
   config.include Features, type: :feature
+  config.infer_base_class_for_anonymous_controllers = false
+  config.infer_spec_type_from_file_location!
+  config.use_transactional_fixtures = false
 end
 
 WebMock.disable_net_connect!(allow_localhost: true)
+ActiveRecord::Migration.maintain_test_schema!
