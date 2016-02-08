0b7b6c8c78f088decb2437e8c8bac9fc42802f78
Hugo Veeger <hugo.veeger@defacto.nl>
2016-02-08 15:06:42 +0100

    refactor subscriptions controller

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -6,41 +6,39 @@ class SubscriptionsController < ApplicationController
   end
 
   def create
-    customer = Stripe::Customer.create(
-      source: params[:stripeToken],
-      plan: "ca-basic",
-      quantity: User.count,
-      email: current_user.email
-    )
-
-    resource.update(
-      stripe_id: customer.id,
-      subscription_id: customer.subscriptions.data[0].id
-    )
+    Subscription.transaction do
+      customer = Stripe::Customer.create(
+        source: params[:stripeToken],
+        plan: "ca-basic",
+        quantity: User.count,
+        email: current_user.email
+      )
+      resource.update(
+        stripe_id: customer.id,
+        subscription_id: customer.subscriptions.data[0].id
+      )
+    end
 
     redirect_to subscription_path
   end
 
   def update
     customer = @stripe_customer
-    card_id = customer.sources.data[0].id 
 
-    customer.sources.retrieve(card_id).delete
+    delete_stripe_default_card
     customer.sources.create(
       source: params[:stripeToken]
-    ) 
+    )
 
     redirect_to subscription_path
   end
 
   def destroy
-    customer = @stripe_customer
-    card_id = customer.sources.data[0].id
-
-    card = customer.sources.retrieve(card_id).delete
-    @subscription.destroy
-
-    customer.subscriptions.retrieve(@subscription.subscription_id).delete
+    @subscription.transaction do
+      delete_stripe_default_card
+      delete_stripe_subscription
+      @subscription.destroy
+    end
 
     redirect_to subscription_path
   end
@@ -53,5 +51,14 @@ class SubscriptionsController < ApplicationController
 
   def set_stripe_customer
     @stripe_customer ||= Stripe::Customer.retrieve(resource.stripe_id) if resource.stripe_id
+  end
+
+  def delete_stripe_default_card
+    card_id = @stripe_customer.sources.data[0].id
+    @stripe_customer.sources.retrieve(card_id).delete
+  end
+
+  def delete_stripe_subscription
+    @stripe_customer.subscriptions.retrieve(@subscription.subscription_id).delete
   end
 end
