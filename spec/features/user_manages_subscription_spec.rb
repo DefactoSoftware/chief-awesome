c6a675b303cd18d318b821d4682de6f9fcd5d370
Hugo Veeger <hugo.veeger@defacto.nl>
2016-02-08 15:06:31 +0100

    add test for changing credit cards

--- spec/features/user_manages_subscription_spec.rb
+++ spec/features/user_manages_subscription_spec.rb
@@ -6,6 +6,7 @@ feature "user manages his or her subscription" do
     stub_create_customer
     stub_retrieve_customer
     stub_retrieve_card
+    stub_new_card
     stub_delete_card
     stub_retrieve_subscription
     stub_cancel_subscription
@@ -20,11 +21,21 @@ feature "user manages his or her subscription" do
     expect(Subscription.last.user.name).to eq("Han Solo") 
   end
 
+  scenario "user changes credit card" do
+    click_link "Change billing info"
+    click_button "Save Card"
+
+    assert_requested stub_delete_card, times: 1
+    assert_requested stub_new_card, times: 1
+  end
+
   scenario "user cancels subscription" do
     click_link "Cancel subscription"
 
     expect(Subscription.where(stripe_id: "cus_7plEoIo9TfMtNg", 
                               subscription_id: "sub_7qWA7rCDQmllX7")).not_to be_present
+    expect(page).not_to have_content("Credit card: **** **** **** ****4242")
+    expect(page).not_to have_content("Subscription owner: Han Solo")
   end
 
   private
